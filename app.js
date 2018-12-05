74aa3d2a2722ff179b562512426bca8ba716d85b
HarlandLohora <lohoraharland@gmail.com>
2018-12-04 20:36:47 -0600

    Casi

--- app.js
+++ app.js
@@ -2,6 +2,16 @@
 var nickName    = document.getElementById('nickName');
 var useNickName = localStorage.getItem('nickName');
 
+if( localStorage.getItem('Panxho') != null ){
+  var allScores = JSON.parse(localStorage.getItem('Panxho'));
+   for( let i = 0; i <= allScores.length-1; i++){
+     $("#score").append(`<p>${allScores[i]}</p>`);
+   }
+
+}else{
+  console.log('Nel');
+  var allScores = [];
+}
 window.onload = function () {
   if( localStorage.getItem('nickName') != null){
     nickName.innerHTML = useNickName;
@@ -17,6 +27,14 @@ function registrarUsuario(){
   }else{
     alert('Ingresa un nombre válido');
   }
+}
+
+function agregarScore(){
+  let score       = document.getElementById('scoreInput').value;
+  allScores.push(score);
+  document.getElementById('scoreInput').value = "";
+  localStorage.setItem("Panxho",JSON.stringify(allScores));
+  console.log(localStorage.getItem("Panxho"));
 }
 
 function clearLS(){
