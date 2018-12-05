677fdcbda461f1ccb3c3c7529d6d39f1b23e3445
HarlandLohora <lohoraharland@gmail.com>
2018-12-04 19:57:58 -0600

    Clear LS

--- app.js
+++ app.js
@@ -1,18 +1,25 @@
+/*Variable globales*/
+var nickName    = document.getElementById('nickName');
+var useNickName = localStorage.getItem('nickName');
+
 window.onload = function () {
   if( localStorage.getItem('nickName') != null){
-
+    nickName.innerHTML = useNickName;
   }
 }
 
 function registrarUsuario(){
   let userName = document.getElementById('userName').value;
-  let nickName = document.getElementById('nickName');
   if( userName != ""){
     /*Guardando en LS*/
     localStorage.setItem('nickName', userName);
-    let useNickName = localStorage.getItem('nickName');
-    nickName.innerHTML = useNickName;
+    nickName.innerHTML = userName;
   }else{
     alert('Ingresa un nombre válido');
   }
+}
+
+function clearLS(){
+  nickName.innerHTML = '';
+  localStorage.clear();
 }
